76283e6e84396acf834a27c3dec663f82ee016fa
unknown <phamexb@gmail.com>
2023-01-05 21:20:10 -0800

    api key

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { ChakraProvider, VStack } from "@chakra-ui/react";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <VStack p={8} margin={3} spacing={10}>
+      </VStack>
+    </ChakraProvider>
   );
 }
 
