bc8a4c1370f8c4ab925ad6c846872b6089b57253
Luan Pham <luanpham@Luans-MacBook-Pro.local>
2023-01-07 23:14:50 -0800

    added searchbar

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,22 +1,46 @@
-import { Box } from '@mui/material';
+import * as React from "react";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import { Box } from "@mui/material";
 
 function Searchbar() {
-
-  const RecipeContext = React.createContext({
-    ingredients: [],
-    addIngredient: ()
-  })
   return (
     <Box
       sx={{
         height: 300,
-        width: '100%',
-        bgcolor: 'black',
+        width: "100%",
       }}
     >
-      Search bar here
+      <Autocomplete
+        id="free-solo-demo"
+        freeSolo
+        options={top100Films.map((option) => option.title)}
+        renderInput={(params) => <TextField {...params} label="freeSolo" />}
+      />
+      <Autocomplete
+        freeSolo
+        id="free-solo-2-demo"
+        disableClearable
+        options={top100Films.map((option) => option.title)}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Search input"
+            InputProps={{
+              ...params.InputProps,
+              type: "search",
+            }}
+          />
+        )}
+      />
     </Box>
   );
 }
 
 export default Searchbar;
+
+// Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
+const top100Films = [
+  { ingredient: "The Shawshank Redemption", year: 1994 },
+  { title: "The Godfather", year: 1972 },
+];
