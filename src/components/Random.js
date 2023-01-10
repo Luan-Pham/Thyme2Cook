191f73b9016826ab0581249f022e618ce0d944f2
unknown <phamexb@gmail.com>
2023-01-10 00:44:26 -0800

    made API call to collect recipes

--- src/components/Random.js
+++ src/components/Random.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-require('dotenv').config();
+
 function Random() {
   const [random, setRandom] = useState([]);
 
@@ -8,12 +8,13 @@ function Random() {
   }, []);
 
   const limit = 10;
-
+  const key = 'd5d1d457c1004156ac6fb0b793d29cc9';
   const getRandom = async () => {
     const api = await fetch(
-      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=${limit}`
+      `https://api.spoonacular.com/recipes/random?apiKey=${key}&number=${limit}`
     );
     const data = await api.json();
+    console.log(data);
     setRandom(data.recipes);
   };
   return <div>Random</div>;
