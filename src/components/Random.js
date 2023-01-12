be2dc2b7a596dcaa19ecb432a0de71e766e4c2c5
unknown <phamexb@gmail.com>
2023-01-11 18:20:38 -0800

    added cards with title and images generated from API

--- src/components/Random.js
+++ src/components/Random.js
@@ -1,4 +1,17 @@
 import React, { useState, useEffect } from 'react';
+import { Card, CardHeader, styled, CardMedia } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
 
 function Random() {
   const [random, setRandom] = useState([]);
@@ -7,7 +20,7 @@ function Random() {
     getRandom();
   }, []);
 
-  const limit = 10;
+  const limit = 5;
   const key = process.env.REACT_APP_SPOON;
   const getRandom = async () => {
     const api = await fetch(
@@ -17,7 +30,23 @@ function Random() {
     console.log(data);
     setRandom(data.recipes);
   };
-  return <div>Random</div>;
+  return (
+    <div>
+      {random.map((recipe) => {
+        return (
+          <Card sx={{ maxWidth: 345 }} key={recipe.id}>
+            <CardHeader title={recipe.title} />
+            <CardMedia
+              component="img"
+              height="194"
+              image={recipe.image}
+              alt={recipe.title}
+            />
+          </Card>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Random;
