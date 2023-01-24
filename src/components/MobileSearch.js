9b18b1eafff1f1f6a5c3aef7ee4a627b06d7b9bb
unknown <phamexb@gmail.com>
2023-01-23 21:53:32 -0800

    restart mobilesearch

--- src/components/MobileSearch.js
+++ src/components/MobileSearch.js
@@ -1,52 +1,41 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import {
   InputLabel,
   FilledInput,
   FormControl,
   Container,
+  Form,
   AppBar,
-  Toolbar,
-} from "@mui/material";
-import styled from "styled-components";
+} from '@mui/material';
+import styled from 'styled-components';
 
 function MobileSearch() {
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState('');
   const navigate = useNavigate();
 
   const submitHandler = (e) => {
     e.preventDefault();
-    navigate("/recipe/" + input);
+    navigate('/recipe/' + input);
   };
 
   return (
     <AppBar position="static">
-      <Container maxWidth="xl">
-        <Form onSubmit={submitHandler}>
-          <FormControl fullWidth sx={{ m: 1 }} variant="filled">
-            <InputLabel>
-              Search for a recipe by ingredient, cuisine, diet, equipment, etc.
-            </InputLabel>
-            <FilledInput
-              onChange={(e) => setInput(e.target.value)}
-              type="text"
-              value={input}
-            />
-          </FormControl>
-        </Form>
-      </Container>
+      <Form onSubmit={submitHandler}>
+        <FormControl fullWidth sx={{ m: 1 }} variant="filled">
+          <InputLabel>
+            Search for a recipe by ingredient, cuisine, diet, equipment, etc.
+          </InputLabel>
+          <FilledInput
+            onChange={(e) => setInput(e.target.value)}
+            type="text"
+            value={input}
+          />
+        </FormControl>
+      </Form>
     </AppBar>
   );
 }
 
 export default MobileSearch;
-
-const Form = styled.form`
-  margin: 0rem 20rem;
-
-  div {
-    width: 100%;
-    position: relative;
-  }
-`;
